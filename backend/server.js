0c85c05579084faf07ec119a152c64822239c6c8
jhonas12000 <67097256+jhonas12000@users.noreply.github.com>
2026-04-11 18:57:55 -0700

    full stack heroku setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const instructorRoutes = require("./routes/instructorRoutes");
 const customerRoutes = require("./routes/customerRoutes");
 const cors = require("cors");
+import path from "path";
 
 
 const app = express();
@@ -17,17 +18,27 @@ app.use("/api/customers", customerRoutes);
 console.log("Starting server...");
 
 // Connect MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/yoga_app")
-  .then(() => console.log("MongoDB Connected"))
+const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/yoga_app";
+
+mongoose.connect(uri)
+  .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-// keep alive fix
-setInterval(() => {}, 1000);
+const __dirname = new URL('.', import.meta.url).pathname;
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
