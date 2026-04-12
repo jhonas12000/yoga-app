fababb5eb11f23aca714c5e7ffe524a265443feb
jhonas12000 <67097256+jhonas12000@users.noreply.github.com>
2026-04-11 19:04:40 -0700

    fix path + serve frontend

--- backend/server.js
+++ backend/server.js
@@ -3,42 +3,34 @@ const mongoose = require("mongoose");
 const instructorRoutes = require("./routes/instructorRoutes");
 const customerRoutes = require("./routes/customerRoutes");
 const cors = require("cors");
-import path from "path";
-
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+
 app.use("/api/instructors", instructorRoutes);
 app.use("/api/customers", customerRoutes);
 
-
 console.log("Starting server...");
 
-// Connect MongoDB
+// MongoDB connection
 const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/yoga_app";
 
 mongoose.connect(uri)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Backend is running...");
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-const __dirname = new URL('.', import.meta.url).pathname;
-
-// Serve frontend
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
