888d98376e689fd8879b6522abc72de3c8967115
jhonas12000 <67097256+jhonas12000@users.noreply.github.com>
2026-04-26 21:24:11 -0700

    Fixed Heroku routing issue

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const frontendPath = path.join(__dirname, "..", "frontend", "dist");
 
 app.use(express.static(frontendPath));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
